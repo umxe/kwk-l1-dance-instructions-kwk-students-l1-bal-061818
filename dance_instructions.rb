c3e747c1cd65f81b27c8ae87a698466d54a41cb6
umxe <u_habiba@rocketmail.com>
2018-06-19 14:05:53 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,20 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches"
+  
+end
+def basic_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts skip_step("left foot")
+}
+your_name = MN
+def say_hello
+  puts "Hello #{your_name}"
+end
+say_hello
---
+++ say_hello.rb
@@ -0,0 +1,5 @@
+
+def say_hello(your_name)
+  puts "Hello #{your_name}"
+end
+say_hello("ume")
